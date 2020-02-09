edd413457025fd41376fa7c05457e34fbd548f3f
wilson azer <wilsonazer@hotmail.com>
2020-02-09 07:38:15 -0300

    add react-router-dom and routs to componet Feed and New

---
+++ frontend/src/pages/Feed.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Feed extends Component{
+    render(){
+        return(
+            <h> welcome to  feed </h>
+        )
+    }
+}
+
+export default Feed
---
+++ frontend/src/pages/New.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class New extends Component{
+    render(){
+        return(
+            <h>News New information</h>
+        )
+    }
+}
+
+export default New
