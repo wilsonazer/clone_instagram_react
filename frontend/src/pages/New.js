b94e6903264b4be1e4d695c90b54a54f3f13cbd9
wilson azer <wilsonazer@hotmail.com>
2020-02-12 10:26:41 -0300

    create new post component

--- frontend/src/pages/New.js
+++ frontend/src/pages/New.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
+import './New.css'
 
 class New extends Component{
     render(){
         return(
-            <h>News New information</h>
+            <form id='new-post'>
+                <input type='file'/>
+                <input type='text' name='author' placeholder='Autor do Post'/>
+                <input type='text' name='place' placeholder='Lugar de origem'/>
+                <input type='text' name='description' placeholder='Descrição do Post'/>
+                <input type='text' name='hastags' placeholder='Hastags'/>
+                <button type='submit' name='send'>Cadastrar</button> 
+            </form>
         )
     }
 }
