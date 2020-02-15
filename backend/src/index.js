3b13ba48764e8d7ac14142c962b3231bbf67098b
wilson azer <wilsonazer@hotmail.com>
2020-02-15 07:40:43 -0300

    add mobile folder with contains a react-native project

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,22 +6,29 @@ const cors = require('cors')
 const routes =  require('../routes')
 
 const app = express()
+//confiração para o uso de real time com socket.io
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 
+//connectando com mongodb usando atlas server
 mongoose.connect('mongodb+srv://wilson:51241314@cluster0-noxyc.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,  
     useUnifiedTopology: true
 })
 
+//middleware para o uso de socket.io em nossas requições
 app.use( ( req, res, next )=>{
     req.io = io
     next()
 })
 
+//Usando o cors para permitir que nossa aplicação converse com apicações externas
 app.use( cors() )
+
 //Adicionando rota para arquivos estático imagens redimencionadas.
 app.use('/files', express.static( path.resolve( __dirname, '..' , 'uploads', 'resized' ) ) )
+
+//middleware para nosso arquivo de rotas
 app.use( routes )
 
 server.listen(3333, ()=>{ 'server running on port: 3333'})
