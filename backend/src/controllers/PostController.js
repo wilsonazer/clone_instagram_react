27259bb3562993a2444e2f37496abb2458c438f0
wilson azer <wilsonazer@hotmail.com>
2020-02-22 09:05:06 -0300

    add comments to PostController

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -6,10 +6,20 @@ const fs = require('fs')
 
 //metodos que serão executados no nosso routes.js
 module.exports = {
+//metodo index lista todos os posts o sinal de menos antes de createdAt ordena em ordena em ordem decrescente    
     async index( req, res ){
         const posts = await Post.find().sort('-createdAt')
         res.json( posts )
     },
+
+/**
+ * Método asincrono store tem muitas coisas acontecendo.  Primeiro usamos a desestruturação para pegar os 
+ * campos vindos do body e também o nosso campo de imagem que é do tipo file. 
+ * Usamos o sharp para redimencionar nossa imagem e o fs.unlinkSync para apagar a imagem original.
+ * Cadastramos as informações recebidas no banco usando nosso Model Post.create().
+ * Criamos um alerta em tempo real passando nossos dados do post para req.io.emmit, o primeiro parâmetro é o nome
+ * para podermos usar essa informação no frontend e o segundo o próprio post.
+ */    
     async store( req, res ){
         const { author, place, description, hastags }  = req.body
         const { filename: image } = req.file
@@ -17,6 +27,7 @@ module.exports = {
         const [ name ] = image.split('.')
         const fileName = `${ name }.jpg`
 
+        //usando sharp para redimensionar imagem
         await sharp( req.file.path )
            .resize(500)
            .jpeg( { quality: 70 })
