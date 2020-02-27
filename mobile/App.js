0983bc1e084f7dea442d2eab6bb9ea0c36229094
wilson azer <wilsonazer@hotmail.com>
2020-02-27 20:20:18 -0300

    add new component, new component

--- mobile/App.js
+++ mobile/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Feed from './pages/Feed'
+import New from './pages/New'
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style = { { color : '#fff'} }>Hi brow let´s code!</Text>
+      <Feed></Feed>
+      <New></New>
     </View>
   );
 }
@@ -12,7 +17,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#333',
     alignItems: 'center',
     justifyContent: 'center',
   },
