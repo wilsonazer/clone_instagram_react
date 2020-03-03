02e9f2040e11b6d7914bb04e67dabdc39f13e916
wilson azer <wilsonazer@hotmail.com>
2020-03-02 21:28:54 -0300

    add navigation to mobile

--- mobile/App.js
+++ mobile/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import Routes from './src/routes'
-
 export default function App() {
   return (
-    <Routes />
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#333',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
