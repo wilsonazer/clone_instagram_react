c89df7c699aabc9227d7c9e051493195f7b7860c
wilson azer <wilsonazer@hotmail.com>
2020-03-02 22:13:14 -0300

    crate routes.js

--- mobile/App.js
+++ mobile/App.js
@@ -1,27 +1,10 @@
-// In App.js in a new project
+import React from 'react';
 
-import * as React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
-
-const Stack = createStackNavigator();
+import Routes from './src/routes'
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Routes />
   );
 }
 
