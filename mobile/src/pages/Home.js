0947b14660338d6da383699c2c2ae5bd3446e413
wilson azer <wilsonazer@hotmail.com>
2020-03-08 09:41:54 -0300

    update home

--- mobile/src/pages/Home.js
+++ mobile/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import { View, Text } from 'react-native'
 
-export default function HomeScreen() {
+export default function Home() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor:'purple' }}>
       <Text style={{ color: '#fff', fontSize:'2em'}}>Home Screen</Text>
