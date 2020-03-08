9fff7952d2fe171172e5048ec38af5a50a58e5d8
wilson azer <wilsonazer@hotmail.com>
2020-03-08 09:38:17 -0300

    add home component

---
+++ mobile/src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor:'purple' }}>
+      <Text style={{ color: '#fff', fontSize:'2em'}}>Home Screen</Text>
+    </View>
+  );
+}
--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,15 +1,8 @@
 import * as React from 'react'
-import { View, Text } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor:'purple' }}>
-      <Text style={{ color: '#fff', fontSize:'2em'}}>Home Screen</Text>
-    </View>
-  );
-}
+import Home from './pages/Home'
 
 const Stack = createStackNavigator();
 
@@ -17,7 +10,7 @@ function routes() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home" component={ Home } />
       </Stack.Navigator>
     </NavigationContainer>
   );
